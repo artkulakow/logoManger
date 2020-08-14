ca16d31ac7cfb064cf23edcc73d5bb9ea81dd528
Art Kulakow <art@kulakow.com>
2020-08-14 13:36:38 -0700

    Added  get all user and modify user api's

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -9,6 +9,37 @@ const cacheUsersFile = 'users.json';
 const moduleURL = new URL(import.meta.url);
 const __dirname = path.dirname(moduleURL.pathname);
 
+const loadAllUsers = (req, res) => {
+    const cFile = __dirname + '/' + cacheDirectory + cacheUsersFile;
+    if (fs.existsSync(cFile)) {
+        console.log('Loading from users cache file')
+        fs.readFile(cFile, (err, cacheData) => {
+            if (err) {
+                console.error('Unable to read cache file: ' + cFile);
+            }
+
+            try {
+                userData = JSON.parse(cacheData);
+
+                res.json({user: userData});
+            }
+            catch(err) {
+                console.error('Unable to convert kits cache file to json: ', err);
+            }
+        })
+    }
+}
+
+export const getAllUsers = (req, res) => {
+    if (userData.length === 0) {
+        loadAllUsers(req, res);
+
+        return;
+    }
+
+    res.json({user: userData});
+}
+
 const sendUserData = (userId, req, res) => {
     const userIndex = userData.findIndex((user) => user.id === userId);
 
@@ -108,6 +139,7 @@ const modifyUserData = (userId, req, res) => {
 }
 
 export const modifyUser = (userId, req, res) => {
+    // make sure the user data is loaded
     if (userData.length == 0) {
         const cFile = __dirname + '/' + cacheDirectory + cacheUsersFile;
         if (fs.existsSync(cFile)) {
@@ -136,6 +168,107 @@ export const modifyUser = (userId, req, res) => {
     modifyUserData(userId, req,res);
 }
 
+const addUser = (req, res) => {
+    const payload = req.body;
+
+    // make sure there is a user name and email address
+    if (!(payload.email && payload.email !== '') || !(payload.userName && payload.userName !== '')) {
+        res.statusMessage = `Missing email or user name`;
+        res.status(404).send(`Missing email or user name`).end()
+
+        return;
+    }
+
+    // make user email address is unique
+    const email = payload.email;
+    const emailIndex = userData.findIndex((user) => user.email === email);
+    if (emailIndex !== -1) {
+        res.statusMessage = `Email ${email} is not unique`;
+        res.status(404).send(`Email ${email} is not unique`).end()
+
+        return;
+    }
+
+    // get the next id
+    let id = 0;
+    if (userData.length > 0) {
+        id = userData.reduce((user, id) => (user.id > id.id) ? user.id: id).id;
+    }
+
+    const dt = new Date();
+    const formatedDate =  (`${
+        dt.getFullYear().toString().padStart(4, '0')}-${
+        (dt.getMonth() + 1).toString().padStart(2, '0')}-${
+        dt.getDate().toString().padStart(2, '0')} ${
+        dt.getHours().toString().padStart(2, '0')}:${
+        dt.getMinutes().toString().padStart(2, '0')}:${
+        dt.getSeconds().toString().padStart(2, '0')}`
+    );
+
+    const newUser = {};
+    newUser.id = id + 1;
+    newUser.email = email;
+    newUser.userName = payload.userName;
+    newUser.firstName = '';
+    if (payload.firstName) {
+        newUser.firstName = payload.firstName;
+    }
+    newUser.lastName = '';
+    if (payload.lastName) {
+        newUser.lastName = payload.lastName;
+    }
+    newUser.units = 'standard';
+    if (payload.units) {
+        newUser.units = payload.units;
+    }
+    newUser.createData = formatedDate;
+    newUser.modifyDate = formatedDate;
+
+    // the the new user
+    userData.push(newUser);
+
+    // update the cache
+    const cFile = __dirname + '/' + cacheDirectory + cacheUsersFile;
+    const jsonString = JSON.stringify(userData);
+    fs.writeFile(cFile, jsonString, err => {
+        if (err) {
+            console.error("Unable to write kits cache file: " + cFile);
+        }
+        else {
+            console.log('Kits Cache File written');
+        }
+    })
+    
+    res.json({user: newUser});
+}
+
 export const createUser = (req, res) => {
-    console.log('create ser!!!')
+    console.log('create user!!!')
+    // make sure the user data is loaded
+    if (userData.length == 0) {
+        const cFile = __dirname + '/' + cacheDirectory + cacheUsersFile;
+        if (fs.existsSync(cFile)) {
+            console.log('Loading from users cache file')
+            fs.readFile(cFile, (err, cacheData) => {
+                if (err) {
+                    console.error('Unable to read cache file: ' + cFile);
+                }
+    
+                try {
+                    const obj = JSON.parse(cacheData);
+                    userData = obj;
+    
+                    // process the data
+                    addUser(req, res);
+                }
+                catch(err) {
+                    console.error('Unable to convert kits cache file to json: ', err);
+                }
+            })
+        }
+
+        return;
+    }
+
+    addUser(req, res);
 }
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,4 +1,4 @@
-import {getUserDetails, modifyUser} from '../controllers/users.js';
+import {getAllUsers, getUserDetails, modifyUser, createUser} from '../controllers/users.js';
 
 import  express from 'express';
 const router = express.Router();
@@ -17,12 +17,22 @@ router.get('/:userId', function(req, res, next) {
     getUserDetails(userId, req, res);
 });
 
+/* Get All Users */
+router.get('/', function(req, res) {
+    getAllUsers(req, res);
+})
+
 /* Modify user setting */
 router.put('/:userId', function(req, res) {
     let userId = req.params.userId;
     userId = parseInt(userId, 10);
 
     modifyUser(userId, req, res);
+})
+
+/* Create user */
+router.post('/', function(req, res) {
+    createUser(req, res);
 })
 
 export default router;
